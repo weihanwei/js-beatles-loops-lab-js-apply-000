d0e8b963f7974e767d4745ffe5975171b99889f5
Hanwei <whwhanwei@gmail.com>
2019-06-20 02:49:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // add solution here
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
